150d7e70692410b8103e2ee71beaab01b1246af5
drstrangeknows <raviworks25@gmail.com>
2021-06-12 19:57:32 +0530

    1) Why is the change being made? Comments for Rain Water problem 2) What has changed to address the problem?
    3) Other concerns
    
    4) Related sprint issue, TT, and/or CM
    
    5) Related design document link
    
    Created with fdt_cr_template v1.02

--- RainTrap.py
+++ RainTrap.py
@@ -1,44 +1,58 @@
 '''
+Basic idea is to find water stored by each bar and add it.
+1st and last bars won't store any water as they form the boundaries
+within which water will be stored. Idea is to calculate for each bar-
+what will be it's left bar & what will be it's right bar. Use 2 arrays
+of the length of original heights array-maxLeft[] & maxRight[]. Iterate
+original array and store the maxLeft for each bar by comparing with
+default maxLeft i.e. 1st item. Siilarly for right bar by comparing 
+by default maxRight i.e. last item. This wasy we established boundary
+for each bar on it's left and right. Now, for each bar water will be
+stored depending on the smaller of the 2 boundaries and that will be
+difference of height of smaller boundary and the bar height.
+Conclusion - eastablish left and right boundary for each bar, calc min
+of the two boundaries for each bar and then calc height difference of the
+boundary from the bar and keep adding to a counter
 
 '''
 
-def findWater(arr, n):
-    maxLeft = [0]*n
-    maxRight = [0]*n
+def findWater(height, n):
+        n=len(height)
+        if n>0:
+            maxLeft = [0]*n
+            maxRight = [0]*n
 
-    leftMax = maxLeft[0] = arr[0]
-    rightMax = maxRight[n-1] = arr[n-1]
-    storedWater = 0
+            leftMax = maxLeft[0] = height[0]
+            rightMax = maxRight[n-1] = height[n-1]
+            storedWater = 0
 
-    for i in range(1, n):
-        if (arr[i]>leftMax):
-            leftMax = arr[i]
+            for i in range(1, n):
+                if (height[i]>leftMax):
+                    leftMax = height[i]
 
-        maxLeft[i] = leftMax
+                maxLeft[i] = leftMax
 
-    for i in range(n-2, -1, -1):
-        if(arr[i]>rightMax):
-            rightMax = arr[i]
+            for i in range(n-2, -1, -1):
+                if(height[i]>rightMax):
+                    rightMax = height[i]
 
-        maxRight[i]=rightMax
+                maxRight[i]=rightMax
 
-    print(maxLeft)
-    print(maxRight)
+            for i in range(0, n):
+                if maxLeft[i]<maxRight[i]:
+                    result = maxLeft[i]-height[i]
+                else:
+                    result = maxRight[i]-height[i]
 
-    for i in range(0, n):
-        if maxLeft[i]<maxRight[i]:
-            result = maxLeft[i]-arr[i]
+                storedWater += result
+            return storedWater
         else:
-            result = maxRight[i]-arr[i]
-
-        storedWater += result
-    return storedWater
+            return 0
 
 def main():
     arr = [5,3,4,6,3,6]
-    n = len(arr)
 
-    print("Total capacity of Water stored ", findWater(arr, n))
+    print("Total capacity of Water stored ", findWater(arr))
 
 if __name__ == "__main__":
     main()
